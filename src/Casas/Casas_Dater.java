522f7439fefaf09785a319febd278bef6de4feae
Federico Simon <claussimon@gmail.com>
2012-08-10 16:06:53 +0200

    Added documentation, used sets instead of weird %2 operators, reversed the order of the set because finalCodes and finalDates were in opposite orders.

--- src/Casas/Casas_Dater.java
+++ src/Casas/Casas_Dater.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Hashtable;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.htmlparser.NodeFilter;
@@ -16,76 +16,97 @@ import org.htmlparser.util.ParserException;
 
 public class Casas_Dater {
 
+    //******
+    //On my Mac or on loaner PC
     public static String onMac = "/Users/fsimon/Desktop/ADAIR/CubaData/";
     public static String onPC = "C:/Documents and Settings/fsimon0/My Documents/CubaData Original/";
+    //******
+    //******
+    // Hashtable to hold both the unique ID and the publish date
     public static Hashtable<String, String> hasht1 = new Hashtable<String, String>();
+    //******
 
     public static void main(String[] args) throws IOException, URISyntaxException, ParserException {
+        //*****
+        //files is an array e.g. {2011-11Sept ... etc}
         File[] files = new File(onMac).listFiles();
+        //*****
+
         iteratorOverAllFiles(files);
         writeTheHashtableToTextFile();
-        //System.out.println(hasht1.toString());
+        System.out.println("Done");
     }
 
+    //*******
+    //this function recursively goes through all of the files {2011-11Sept, ... etc}
+    //and if (the file is a folder, the file doesn't contain "permutas" or "renta")
+    //then it sends the file to extract HTML
     public static void iteratorOverAllFiles(File[] files) throws URISyntaxException, ParserException {
         for (File file : files) {
             if (file.isDirectory() && !file.getName().equals("permutas") && !file.getName().equals("renta")) {
-                //System.out.println(file.getName());
                 iteratorOverAllFiles(file.listFiles());
             } else {
-                if ((file.getName().equals("buscador.html") || file.getName().equals("index.html")) 
+                if ((file.getName().equals("buscador.html") || file.getName().equals("index.html"))
                         && !file.getAbsolutePath().contains("renta")) {
-                    System.out.println(file.getAbsolutePath());
+                    //System.out.println(file.getAbsolutePath());
                     extractFromHTML(file.getAbsolutePath());
-                    //System.out.println("something sent to codeExtractEntire");
                 }
             }
         }
     }
+    //********
 
+    //********
+    //
     public static void writeTheHashtableToTextFile() throws IOException {
         File theAllTXTFile = new File(onMac + "Casas_Date_Dictionary.csv");
-        //"C:/Documents and Settings/fsimon0/My Documents/CubaData Original/Rentas_Date_Dictionary.csv"
         FileWriter allFileWriter = null;
+        
         try {
             allFileWriter = new FileWriter(theAllTXTFile);
         } catch (IOException e1) {
         }
+        
         String toWrite = hasht1.toString();
-        toWrite = toWrite.replace("{", "");
-        toWrite = toWrite.replace("}", "");
+        toWrite = toWrite.replaceAll("[{ }]", "");
         toWrite = toWrite.replace(",", "\n");
+        
         allFileWriter.write(toWrite);
         try {
             allFileWriter.close();
         } catch (IOException e) {
         }
     }
+    //********
 
+    //********
+    //
     public static void extractFromHTML(String theURL) throws URISyntaxException, ParserException {
 
+        //********
+        //This extracts the price, publish date
         FilterBean thePriceFilter = new FilterBean();
         HasAttributeFilter priceattr = new HasAttributeFilter("class", "renta_precio");
         thePriceFilter.setFilters(new NodeFilter[]{priceattr});
         thePriceFilter.setURL(theURL);
         String rawPExtract = thePriceFilter.getText();
-        String priceStr="";
-        if(rawPExtract!=null){
+        String priceStr = "";
+        if (rawPExtract != null) {
             priceStr = rawPExtract.replace("\r\n", "");
         }
         priceStr = priceStr.replace(")", ")\n").replace("-", "- \n");
-        //System.out.println("jhjbj"+priceStr);
 
         FilterBean pubFilter = new FilterBean();
-        HasAttributeFilter pubattr = new HasAttributeFilter("class", "renta_publicado");
+        HasAttributeFilter pubattr = new HasAttributeFilter("class", "renta_publicado");// <== WHY RENTA???
         pubFilter.setFilters(new NodeFilter[]{pubattr});
         pubFilter.setURL(theURL);
         String rawPubExtract = pubFilter.getText();
-        //System.out.println("ASDASDASDASDASDASDASDASDA" + rawPubExtract)
+        //System.out.println("small" + rawPubExtract);
+        //********
 
-
+        //********
+        //This makes an array of all the links on the html file
         String linkString = "";
-
         LinkBean linkBean = new LinkBean();
         linkBean.setURL(theURL);
         URL[] links = linkBean.getLinks();
@@ -93,45 +114,40 @@ public class Casas_Dater {
             linkString = linkString + link.toString() + "\n";
             //System.out.println(linkString);
         }
-
         String[] linkStringArray = linkString.split("\n");
-        String toBeFinalArray = "";
-
+        //********
+        
+        //********
+        //This makes an array of all of the unique IDs
+        Set <String> toBeFinalSet = new TreeSet <String>();
+        Pattern pattern = Pattern.compile("!(.*?).htm"); //changed this from .html.
         for (String line : linkStringArray) {
             if (line.contains("file:") && !line.contains("index") && !line.contains("administrar")) {
-                Pattern pattern = Pattern.compile("!(.*?).html");
                 Matcher matcher = pattern.matcher(line);
                 if (matcher.find()) {
                     //System.out.println(matcher.group(1));
-                    toBeFinalArray = toBeFinalArray + matcher.group(1) + "\n";
+                    toBeFinalSet.add(matcher.group(1));
                 }
             }
         }
+        //********
 
-        String[] finalArrayWithDuplicates = toBeFinalArray.split("\n");;
-        String tobeUnique = "";
-
-        for (int tobeeven = 0; tobeeven < finalArrayWithDuplicates.length; tobeeven++) {
-            if (tobeeven % 2 == 0) {
-                tobeUnique = tobeUnique + finalArrayWithDuplicates[tobeeven] + "\n";
-            }
-        }
-
-        String[] finalArrayofCodes = tobeUnique.split("\n");
-
+        String[] finalArrayofCodes = toBeFinalSet.toArray(new String[toBeFinalSet.size()]);
+        //System.out.println("here:" + rawPubExtract);
         String[] finalArrayofDates = rawPubExtract.split("\n");
 
-        System.out.println(finalArrayofCodes.length + "," + finalArrayofDates.length);
+        //System.out.println(finalArrayofCodes.length + "," + finalArrayofDates.length);
 
         if (finalArrayofCodes.length == finalArrayofDates.length) {
-            //System.out.println("CATASTROPHIC FAILURE");
             for (int w = 0; w < finalArrayofCodes.length; w++) {
-                String key = finalArrayofCodes[w];
+                //System.out.println(finalArrayofCodes.length + "-" + w + "=" + (finalArrayofCodes.length-w));
+                String key = finalArrayofCodes[finalArrayofCodes.length-w-1];
                 String value = finalArrayofDates[w];
-                System.out.println(key + ","+ value);
+                //System.out.println(key + "," + value);
                 hasht1.put(key, value);
                 //System.out.println(finalArrayofCodes[w] + "," +finalArrayofDates[w]);
             }
         }
     }
+    //*******
 }
